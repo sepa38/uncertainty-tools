aff8b469879e5a6667709998470ee6d0e1a3f932
sepa38 <82813488+sepa38@users.noreply.github.com>
2024-10-30 15:54:46 +0900

    test: add unit tests for UncertainValue addition

---
+++ tests/test_addition.py
@@ -0,0 +1,56 @@
+import unittest
+import sys
+import os
+sys.path.append(os.pardir)
+
+from uncertain_value import UncertainValue # type: ignore
+
+
+class TestUncertainValueAddition(unittest.TestCase):
+
+    def test_addition_both_uncertain(self):
+        x1 = UncertainValue(10, 0.5)
+        x2 = UncertainValue(20, 0.3)
+        result = x1 + x2
+        self.assertEqual(repr(result), "30.0 ± 0.6")
+
+    def test_addition_one_uncertain_zero_error(self):
+        x1 = UncertainValue(10, 0.5)
+        x2 = UncertainValue(20, 0)
+        result = x1 + x2
+        self.assertEqual(repr(result), "30.0 ± 0.5")
+
+    def test_addition_with_constant(self):
+        x1 = UncertainValue(10, 0.5)
+        result = x1 + 5
+        self.assertEqual(repr(result), "15.0 ± 0.5")
+
+        result = 5 + x1
+        self.assertEqual(repr(result), "15.0 ± 0.5")
+
+    def test_addition_with_negative_values(self):
+        x1 = UncertainValue(-10, 0.5)
+        x2 = UncertainValue(-20, 0.3)
+        result = x1 + x2
+        self.assertEqual(repr(result), "-30.0 ± 0.6")
+
+    def test_addition_large_value_small_error(self):
+        x1 = UncertainValue(1e6, 1)
+        x2 = UncertainValue(2e6, 2)
+        result = x1 + x2
+        self.assertEqual(repr(result), "3000000 ± 2")
+
+    def test_addition_large_value_difference(self):
+        x1 = UncertainValue(1e-2, 1e-3)
+        x2 = UncertainValue(1e6, 1e-3)
+        result = x1 + x2
+        self.assertEqual(repr(result), "1000000.010 ± 0.001")
+
+    def test_addition_large_error_difference(self):
+        x1 = UncertainValue(10, 1e-3)
+        x2 = UncertainValue(20, 5)
+        result = x1 + x2
+        self.assertEqual(repr(result), "30 ± 5")
+
+if __name__ == '__main__':
+    unittest.main()
