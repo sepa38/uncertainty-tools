9dcfc11020dbec0c738f49619ef0dbbefa45b4ce
sepa38 <82813488+sepa38@users.noreply.github.com>
2024-11-13 13:09:21 +0900

    feat: four arithmetic operations on Array

--- src/array.py
+++ src/array.py
@@ -22,6 +22,34 @@ class Array:
     def __repr__(self):
         return f"Array({self.values})"
 
+    def __add__(self, other):
+        if not isinstance(other, Array):
+            raise ValueError("Operand must be an instance of Array.")
+        if len(self) != len(other):
+            raise ValueError("Arrays must have the same length.")
+        return Array([self[i] + other[i] for i in range(len(self))])
+
+    def __sub__(self, other):
+        if not isinstance(other, Array):
+            raise ValueError("Operand must be an instance of Array.")
+        if len(self) != len(other):
+            raise ValueError("Arrays must have the same length.")
+        return Array([self[i] - other[i] for i in range(len(self))])
+
+    def __mul__(self, other):
+        if not isinstance(other, Array):
+            raise ValueError("Operand must be an instance of Array.")
+        if len(self) != len(other):
+            raise ValueError("Arrays must have the same length.")
+        return Array([self[i] * other[i] for i in range(len(self))])
+
+    def __truediv__(self, other):
+        if not isinstance(other, Array):
+            raise ValueError("Operand must be an instance of Array.")
+        if len(self) != len(other):
+            raise ValueError("Arrays must have the same length.")
+        return Array([self[i] / other[i] for i in range(len(self))])
+
     def append(self, value):
         if isinstance(value, (UncertainValue, int, float)):
             value = value if isinstance(value, UncertainValue) else UncertainValue(value)
